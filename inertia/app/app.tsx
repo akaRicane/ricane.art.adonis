e9fcae5f587aaa80403b69a1db28bc73788c1dd8
akaRicane <philippe.guelen@gmail.com>
2024-03-26 00:23:00 +0100

    init boilerplate adonijs+inertia+react+ts+tailwindcss

---
+++ inertia/app/app.tsx
@@ -0,0 +1,20 @@
+import { resolvePageComponent } from '@adonisjs/inertia/helpers'
+import { createInertiaApp } from '@inertiajs/react'
+import { hydrateRoot } from 'react-dom/client'
+import '../css/app.css'
+
+const appName = import.meta.env.VITE_APP_NAME || 'AdonisJS'
+
+createInertiaApp({
+  progress: { color: '#5468FF' },
+
+  title: (title) => `${title} - ${appName}`,
+
+  resolve: (name) => {
+    return resolvePageComponent(`../pages/${name}.tsx`, import.meta.glob('../pages/**/*.tsx'))
+  },
+
+  setup({ el, App, props }) {
+    hydrateRoot(el, <App {...props} />)
+  },
+})
